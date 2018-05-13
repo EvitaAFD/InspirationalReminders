8ad634612d8f7675bf172b8808dd0cca74d99fcb
EVE DENISON <evedenison@gmail.com>
2018-05-13 16:56:34 -0700

    create datasource and delegate protocols for collection view

---
+++ InspirationalReminders/InspirationalReminders/QuoteCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  QuoteCollectionViewCell.swift
+//  InspirationalReminders
+//
+//  Created by Eve Denison on 5/13/18.
+//  Copyright © 2018 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class QuoteCollectionViewCell: UICollectionViewCell {
+    
+    //Create a label to display on the collection view cell
+    @IBOutlet weak var cellLabel: UILabel!
+    
+}
---
+++ InspirationalReminders/InspirationalReminders/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  InspirationalReminders
+//
+//  Created by Eve Denison on 5/13/18.
+//  Copyright © 2018 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    //Create reuse identifier for cell
+    let cell = "cell"
+    var inspirationalQuotes = ["1", "2", "3", "4"]
+    
+    //Define number of cells in collection view
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Return quotes in cell
+        return self.inspirationalQuotes.count
+    
+    }
+    
+    //Create interactive movement of item at selected index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! QuoteCollectionViewCell
+        
+        cell.cellLabel.text = self.inspirationalQuotes[indexPath.item]
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Cell selected at \(indexPath.item)")
+    }
+
+}
+
