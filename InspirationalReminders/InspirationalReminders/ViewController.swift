009583d2cb0588d0753ca98301180cd9e1959fb9
EVE DENISON <evedenison@gmail.com>
2018-05-14 11:34:23 -0700

    refactor action alter

--- InspirationalReminders/InspirationalReminders/ViewController.swift
+++ InspirationalReminders/InspirationalReminders/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     //Create reuse identifier for cell
     let cell = "cell"
-    var inspirationalQuotes = ["\"No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.\" - Buddha", "\"Give, even if you only have a little.\" - Buddah", "\"Even as a solid rock is unshaken by the wind, so are the wise unshaken by praise or blame.\" - Buddah", "\"Nothing can harm you as much as your own thoughts unguarded.\" - Buddah"]
+    let inspirationalQuotes = ["\"No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.\" - Buddha",
+        "\"Give, even if you only have a little.\" - Buddah",
+        "\"Even as a solid rock is unshaken by the wind, so are the wise unshaken by praise or blame.\" - Buddah",
+        "\"Nothing can harm you as much as your own thoughts unguarded.\" - Buddah"]
     
     //Define number of cells in collection view
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -25,7 +29,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     //Create interactive movement of item at selected index path
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! QuoteCollectionViewCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseCellIdentifier", for: indexPath) as? QuoteCollectionViewCell else {
+            
+            return UICollectionViewCell()
+            
+        }
         
         cell.cellLabel.text = self.inspirationalQuotes[indexPath.item]
         
@@ -34,18 +42,35 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        let selectedQuote = self.inspirationalQuotes[indexPath.item]
+        
         //Set up alert to confirm or cancel selection of quote user wants to be reminded of
         let alertController = UIAlertController(title: "Great Selection!", message:
-            "Would you like to be reminded of this fab quote?", preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default,handler: nil))
+            "Would you like to be reminded of this fab quote: \(selectedQuote)?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default) { (action) in
+            
+        }
+        
+        
+        let dismissAction = UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.cancel) { (action) in
+            
+            
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(dismissAction)
         
       /* When confirm is pressed define content using UNMutableNotificationContent() to contain selected quote, likely using seleced indexPath, next set time for local notification to occur using DateComponents() to set one minute from selection, third, create and register request with the notification system, lastly be sure to cancel the request */
-        
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.cancel, handler: nil))
         
         self.present(alertController, animated: true, completion: nil)
         print("Cell selected at \(indexPath.item)")
     }
-
+    
+    func scheduleNotification (withQuote quote: String) {
+        
+        
+        
+    }
 }
 
